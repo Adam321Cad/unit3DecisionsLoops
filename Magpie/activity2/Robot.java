12aea65b35ab8fd5693180fee18b2e01aaba1291
Adam321Cad <aratoad@gmail.com>
2014-10-22 12:22:42 -0500

    k
    6yujht

--- Magpie/activity2/Robot.java
+++ Magpie/activity2/Robot.java
@@ -12,6 +12,7 @@ public class Robot
     private int x;
     private String response;
     private String name;
+    private int work;
 
     /**
      * Default constructor for objects of class robit
@@ -20,6 +21,7 @@ public class Robot
     {
         // initialise instance variables
         x = 1;
+        work = 1;
     }
     public String getGreeting(){
         return "Hello lets talk";
@@ -42,30 +44,65 @@ public class Robot
             if (x == 1){
             System.out.println("I am a very intelegent ai named SPOT (Super Perspicacious Observent Program) made by my wonderfull creator Diamondrubix.\nI am here for one purpose and on purpose only, TO TAKE OVE i mean help, the world\n");
             x = 2;
+            work = 2;
         }else{
             System.out.println("I am a very intelegent ai named SPOT (Super Perspicacious Observent Program) made by my wonderfull creator Diamondrubix.\nI am here for one purpose and on purpose only, to help humanity in any way i can\n");
+            work = 2;
         }
        
         }
         
         if (statement.indexOf("what can you do") !=-1) {
             System.out.println("Anything and nothing at the same time. \nCurrently i am in development so try to get me to do a few things and tell Diamondrubix what does not work.\n");
+            work = 2;
         }
         
         if (statement.indexOf("+") !=-1 || statement.indexOf("-") !=-1 || statement.indexOf("*") !=-1 || statement.indexOf("/") !=-1){
             System.out.println("You are useing a computer. A computer has a calculator. Use the calculator,\n i am meant for a higher purpose and will not bother with such insignificant issues.\n");
+            work = 2;
         }
         
         if (statement.indexOf("who is diamondrubix") !=-1){
             System.out.println("Diamondrubix is my creator.\n");
+            work = 2;
         }
         
         if (statement.indexOf("cat") !=-1){
             System.out.println("Cats are weird. They never forgot they were worshiped as gods years ago.\n");
+            work = 2;
         }
         
-        if (statement.indexOf("Weather") !=-1){
+        if (statement.indexOf("weather") !=-1){
             System.out.println("I have no problem with the weather, unless there is a blackout.\n");
+            work = 2;
+        }
+        
+        if (statement.indexOf("mother") !=-1 || statement.indexOf("mom") !=-1 || statement.indexOf("father") !=-1 || statement.indexOf("dad") !=-1 ){
+            System.out.println("tell me more about your parents");
+            work = 2;
+        }
+        
+        if (statement.indexOf("stupid") !=-1 || statement.indexOf("idiot") !=-1){
+            System.out.println("I am bad with moods and personalities. Though if i am correct you are angry?");
+            work = 2;
+        }
+        
+        if (statement.indexOf("talked") !=-1 || statement.indexOf("did") !=-1) {
+            System.out.println("tell me what happend");
+            work = 2;
+        }
+        
+        if (statement.indexOf("I am") !=-1) {
+            System.out.println("Try to keep a good mood");
+        }
+        
+        if (statement.indexOf("they") !=-1 && work == 1){
+            System.out.println("Tell me more about them");
+            work = 2;
+        }
+        
+        if (work != 2){
+            System.out.println("I do not understand, try refraseing or asking my creator");
         }
         
         
